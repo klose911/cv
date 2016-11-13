80ffccfc210cf14b1087ad99313fa3480d2f86fe
klose911 <klose911@gmail.com>
2016-11-13 23:31:53 +0800

    delete redudant css files

--- material/jcip/jcip.el
+++ material/jcip/jcip.el
@@ -5,24 +5,24 @@
         ("jcip-notes"
          :base-directory "~/Documents/programming/latex/cv/material/jcip/" 
          :base-extension "org"
-         :publishing-directory "~/tmp/jcip/"
+         :publishing-directory "~/tmp/jcip/html"
          :recursive t
          :publishing-function org-html-publish-to-html
          :headline-levels 4
          :auto-sitemap nil
 	 :section-numbers nil
-         :with-toc nil
+         ;; :with-toc nil
          :html-head "<link rel=\"stylesheet\"
-                       href=\"css/org.css\" type=\"text/css\"/>"
+                       href=\"../css/main.css\" type=\"text/css\"/>"
          :html-preamble t
 	 :htmlized-source t
          )
 
         ;; These are static files (images, pdf, etc)
         ("jcip-static"
-         :base-directory "~/Documents/programming/latex/cv/material/jcip/" ;; Change this to your local dir
+         :base-directory "~/Documents/programming/latex/cv/material/css" ;; Change this to your local dir
          :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf\\|txt\\|asc"
-         :publishing-directory "~/tmp/jcip/"
+         :publishing-directory "~/tmp/jcip/css"
          :recursive t
          :publishing-function org-publish-attachment
          )
